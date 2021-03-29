995c5da1ba1fd66e38b2f6e4a77657581a3802a8
Allmight-ccc <1804475055@qq.com>
2021-03-29 13:43:32 +0800

    master

---
+++ data_structure/table_structure/link.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// 单向链表，实现增删改查功能
+
+
+// 
+struct node {
+    int data;
+    node *next;
+};
+
+int main() {
+
+    return 0;
+}
--- data_structure/table_structure/list.cc
+++ data_structure/table_structure/list.cc
@@ -2,7 +2,15 @@
 
 using namespace std;
 
+// 单向顺序表，实现增删改查功能
+
+
+// 顺序表结构
+struct list {
+    int *data;
+};
+
 int main() {
-    cout << "Hello World" << endl;
+
     return 0;
 }
