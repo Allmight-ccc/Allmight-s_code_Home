acae8239a89975839eb416a17ed9c7f06b5dbe3f
Allmight-ccc <1804475055@qq.com>
2021-03-30 00:01:02 +0800

    master

--- data_structure/table_structure/list&link/linkdoble.cc
+++ data_structure/table_structure/list&link/linkdoble.cc
@@ -5,19 +5,41 @@ using namespace std;
 typedef struct node
 {
     int data;
-    node *preious, *next;
+    node *previous, *next;
 }node, *linkdList;
 
 linkdList init() {
     linkdList l = new node[sizeof(node)];
-    l->preious = NULL;
+    l->previous = NULL;
     l->next = NULL;
     return l;
 }
 
 void insert_node(linkdList l, int data) {
-    
+    node* n = new node[sizeof(node)];
+    n->data = data;
+    l->next = n;
+    n->previous = l;
+    n->next = NULL;
 }
+
+void delete_node(linkdList l, int data) {
+    node* p = l->next;
+    while (p) {
+        if(p->next != NULL && p->next->data == data) {
+            //delete code
+        }else if(p->data == data) {
+            
+        }
+        p = p->next;
+    }
+}
+
+void update_node(linkdList l, int data) {
+
+}
+
+
 
 int main() {
     return 0;
