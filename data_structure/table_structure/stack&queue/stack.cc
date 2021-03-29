f4203af429dd2a140893b5c0ea0dbcf49c40f963
Allmight-ccc <1804475055@qq.com>
2021-03-29 15:44:38 +0800

    master

---
+++ data_structure/table_structure/stack&queue/stack.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+#define MAXSIZE 100
+
+struct stack {
+    int *data;
+    int top;
+};
+
+stack* init_stack() {
+    stack *s = new stack[sizeof(stack)];
+    s->data = new int[MAXSIZE];
+    s->top = -1;
+}
+
+void posh(stack* s, int data) {
+    if (s->top == MAXSIZE) return;
+
+    s->data[++s->top] = data;
+}
+
+int pop(stack* s) {
+    if (s->top == MAXSIZE) return;
+
+    return s->data[s->top--];
+}
+
+void print(stack* s) {
+    for (int i = 0; i < s->top; i++) {
+        cout << s->data[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    return 0;
+}
