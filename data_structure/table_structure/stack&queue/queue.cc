c8c05af953287b54fff6a7fedb92c37cd4333f06
Allmight-ccc <1804475055@qq.com>
2021-03-29 22:04:31 +0800

    master

--- data_structure/table_structure/stack&queue/queue.cc
+++ data_structure/table_structure/stack&queue/queue.cc
@@ -6,15 +6,48 @@ using namespace std;
 
 struct queue {
     int *data;
-    int size;
+    int head, tail;
 };
 
 queue* init_queue() {
     queue* q = new queue[sizeof(queue)];
     q->data = new int[MAXSIZE];
-    q->size = 0;
+    q->head = q->tail = 0;
+}
+
+void push(queue* q, int data) {
+    q->data[q->tail++] = data;
+}
+
+int pop(queue* q) {
+    if(q->tail <= q->head) return -1;
+    return q->data[q->head++];
+}
+
+void print_queue(queue* q) {
+    for(int i = q->head; i < q->tail; ++i) {
+        cout << q->data[i] << " ";
+    }
+    cout << endl;
 }
 
 int main() {
+    int data;
+    queue* q = init_queue();
+    for(int i = 0; i < 5; ++i) {
+        cout << ">>> ";
+        cin >> data;
+        push(q, data);
+    }
+    print_queue(q);
+    
+
+
+    while (true) {
+        data = pop(q);
+        if(data == -1) break;
+        cout << "poped: " << data << endl;
+    }
+
     return 0;
 }
