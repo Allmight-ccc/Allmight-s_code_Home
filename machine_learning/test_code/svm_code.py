3ec3a1108080d4eae368bd4dc0faf44749d72636
Allmight-ccc <1804475055@qq.com>
2021-03-29 10:24:36 +0800

    master

--- machine_learning/test_code/svm_code.py
+++ machine_learning/test_code/svm_code.py
@@ -17,13 +17,12 @@ y = data_set.target  # label
 target_names = data_set.target_names
 
 # 画散点图
+import matplotlib.pyplot as plt
 
 f1 = plt.figure(1)
 
-p1 = plt.scatter(X[y == 0, 0], X[y == 0, 1], color='r',
-                 label=target_names[0])  # feature
-p2 = plt.scatter(X[y == 1, 0], X[y == 1, 1], color='g',
-                 label=target_names[1])  # feature
+p1 = plt.scatter(X[y == 0, 0], X[y == 0, 1], color='r', label=target_names[0])  # feature
+p2 = plt.scatter(X[y == 1, 0], X[y == 1, 1], color='g', label=target_names[1])  # feature
 plt.xlabel(feature_names[0])
 plt.ylabel(feature_names[1])
 plt.legend(loc='upper right')
@@ -32,14 +31,18 @@ plt.grid(True, linewidth=0.3)
 plt.show()
 
 # 数据规范化
+from sklearn import preprocessing
 
 normalized_X = preprocessing.normalize(X)
 
 # 模型拟合和测试
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn import svm
+import numpy as np
 
 # 训练集和测试集的生成
-X_train, X_test, y_train, y_test = train_test_split(
-    normalized_X, y, test_size=0.5, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(normalized_X, y, test_size=0.5, random_state=0)
 
 # 模型拟合，测试，可视化
 # 基于线性内核和高斯内核
@@ -68,6 +71,7 @@ for fig_num, kernel in enumerate(('linear', 'rbf')):
 # BP net for classification on breast_cancer data set
 
 # 加载数据
+from sklearn.datasets import load_breast_cancer
 
 data_set = load_breast_cancer()
 
@@ -77,10 +81,12 @@ y = data_set.target  # label
 target_names = data_set.target_names
 
 # data normalization
+from sklearn import preprocessing
 
 normalized_X = preprocessing.normalize(X)
 
 # 构建数据
+from pybrain.datasets import ClassificationDataSet  # 分类数据专用数据集工具包
 
 ds = ClassificationDataSet(30, 1, nb_classes=2, class_labels=y)
 for i in range(len(y)):
@@ -91,21 +97,22 @@ ds.calculateStatistics()
 tstdata_temp, trndata_temp = ds.splitWithProportion(0.5)
 tstdata = ClassificationDataSet(30, 1, nb_classes=2)
 for n in range(0, tstdata_temp.getLength()):
-    tstdata.appendLinked(tstdata_temp.getSample(
-        n)[0], tstdata_temp.getSample(n)[1])
+    tstdata.appendLinked(tstdata_temp.getSample(n)[0], tstdata_temp.getSample(n)[1])
 # 定义训练集，  输入维度是30，target是1，有2类，均是2d向量
 trndata = ClassificationDataSet(30, 1, nb_classes=2)
 for n in range(0, trndata_temp.getLength()):
-    trndata.appendLinked(trndata_temp.getSample(
-        n)[0], trndata_temp.getSample(n)[1])
+    trndata.appendLinked(trndata_temp.getSample(n)[0], trndata_temp.getSample(n)[1])
 trndata._convertToOneOfMany()
 tstdata._convertToOneOfMany()
 
 # 建立训练
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure.modules import SoftmaxLayer
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.utilities import percentError
 
 n_hidden = 500
-bp_nn = buildNetwork(trndata.indim, n_hidden,
-                     trndata.outdim, outclass=SoftmaxLayer)
+bp_nn = buildNetwork(trndata.indim, n_hidden, trndata.outdim, outclass=SoftmaxLayer)
 trainer = BackpropTrainer(bp_nn,
                           dataset=trndata,
                           verbose=True,
@@ -116,6 +123,7 @@ err_train, err_valid = trainer.trainUntilConvergence(maxEpochs=1000,
                                                      validationProportion=0.25)
 
 # 收敛曲线用于累积BP算法的过程
+import matplotlib.pyplot as plt
 
 f1 = plt.figure(1)
 plt.plot(err_train, 'b', err_valid, 'r')
@@ -135,6 +143,8 @@ data_file_datatype = "result/datatypes.csv"
 
 # C4.5用于乳腺癌数据集的分类
 
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
 
 # 生成数据集
 data_set = load_breast_cancer()
@@ -143,12 +153,11 @@ feature_names = data_set.feature_names
 y = data_set.target  # label
 target_names = data_set.target_names
 
+from sklearn.model_selection import train_test_split
 
 # 生成训练和测试集
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.5, random_state=0)
-X_train, X_valid, y_train, y_valid = train_test_split(
-    X_train, y_train, test_size=0.5, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=0.5, random_state=0)
 
 df_train = pd.DataFrame(X_train)
 df_train.columns = feature_names
@@ -166,10 +175,12 @@ df_test['class'] = y_test
 df_test.to_csv(data_file_test)
 
 # 学习和训练
+import decision_tree
 
 decision_tree.main()
 
 # 结果
+from sklearn import metrics
 
 df_result = pd.read_csv(open('results.csv', 'r'))
 y_pred = df_result['class'].values
