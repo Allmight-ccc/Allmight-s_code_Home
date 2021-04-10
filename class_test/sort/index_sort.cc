575c89a5e130aa29559c3d17a1aa69dfd7af10e9
Allmight-ccc <1804475055@qq.com>
2021-04-10 11:44:33 +0800

    master

---
+++ class_test/sort/index_sort.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+void print_array(int* arr, int size) {
+    for(int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int* copy_array(int* arr, int size) {
+    int* new_arr = new int[size];
+    for(int i = 0; i < size; ++i) {
+        new_arr[i] = arr[i];
+    }
+}
+
+void sort(int* arr, int size) {
+    int index[size] = {-1};
+    int min = 0;
+    int* new_arr = copy_array(arr, size);
+    for(int i = 0; i < size; ++i) {
+        min = index[i]-1;
+        for(int j = 0; j < size; ++j) {
+            if(new_arr[j] == 0) continue;
+            if(new_arr[j] < new_arr[min]) {
+                min = j;
+            }
+        }
+        index[i] = min;
+        cout << "index[i]: " << index[i] << " i: " << i << endl;
+        new_arr[min] = 0;
+        cout << "第" << i << "次排序：";
+        print_array(new_arr, size);
+    }
+
+    // for(int i = 0; i < size; ++i) {
+    //     cout << arr[index[i]] << " ";
+    // }
+    // cout << endl;
+    delete(new_arr);
+}
+
+int main() {
+    int arr[] = {54, 34, 21, 213, 100, 97, 333, 249, 1, 43, -132, 67};
+    sort(arr, sizeof(arr)/4);
+    print_array(arr, sizeof(arr)/4);
+    return 0;
+}
