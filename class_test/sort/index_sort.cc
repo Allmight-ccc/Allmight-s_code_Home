ebbc17be8f2954a4462da5e427821a8d3f7b43f8
Allmight-ccc <1804475055@qq.com>
2021-04-10 12:08:25 +0800

    master

--- class_test/sort/index_sort.cc
+++ class_test/sort/index_sort.cc
@@ -9,20 +9,28 @@ void print_array(int* arr, int size) {
     cout << endl;
 }
 
+void print_array(int* arr, int size, int* index) {
+    for(int i = 0; i < size; ++i) {
+        cout << arr[index[i]] << " ";
+    }
+    cout << endl;
+}
+
 int* copy_array(int* arr, int size) {
     int* new_arr = new int[size];
     for(int i = 0; i < size; ++i) {
         new_arr[i] = arr[i];
     }
+    return new_arr;
 }
 
+int index[11] = {1};
 void sort(int* arr, int size) {
-    int index[size] = {-1};
     int min = 0;
-    int* new_arr = copy_array(arr, size);
-    for(int i = 0; i < size; ++i) {
+    int *new_arr = copy_array(arr, size);
+    for(int i = 0; i < size; i++) {
         min = index[i]-1;
-        for(int j = 0; j < size; ++j) {
+        for(int j = 0; j < size; j++) {
             if(new_arr[j] == 0) continue;
             if(new_arr[j] < new_arr[min]) {
                 min = j;
@@ -34,17 +42,12 @@ void sort(int* arr, int size) {
         cout << "第" << i << "次排序：";
         print_array(new_arr, size);
     }
-
-    // for(int i = 0; i < size; ++i) {
-    //     cout << arr[index[i]] << " ";
-    // }
-    // cout << endl;
-    delete(new_arr);
 }
 
 int main() {
     int arr[] = {54, 34, 21, 213, 100, 97, 333, 249, 1, 43, -132, 67};
     sort(arr, sizeof(arr)/4);
     print_array(arr, sizeof(arr)/4);
+    print_array(arr, sizeof(arr)/4, index);
     return 0;
 }
