6f618ae4b7cadacaa08682787a536005bbf65398
Allmight-ccc <1804475055@qq.com>
2021-04-30 13:29:12 +0800

    master

---
+++ data_structure/graph_structure/test/dijkstra.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+#define N 4
+#define INF 0xFFFFFFFFb
+
+void initDist(int c[][N], int dist[], int start) {
+    for(int i = 0; i < N; ++i) {
+        dist[i] = c[start][i];
+    }
+}
+
+int getMinDist(int c[][N], int flag[], int dist[], int start) {
+    int min = INF, k;
+}
+
+int getMin(int a, int b) {
+    return a < b ? a : b;
+}
+
+void updateDist(int c[][N], int flag[], int dist[], int k, int start) {
+    for(int i = 0; i < N; ++i) {
+        int min = getMin(dist[k] + c[k][i], c[start][i]);
+        
+    }
+}
+
+void dijkstra(char V[], int c[][N], int dist[], int start) {
+    initDist(c, dist, start);
+    int flag[N] = {0};
+    flag[start] = 1;
+    for(int i = 0; i < N-1; ++i) {
+        int k = getMinDist(c, flag, dist, start);
+        flag[k] = 1;
+        updateDist(c, flag, dist, k, start);
+    }
+}
+
+int main() {
+    return 0;
+}
--- data_structure/graph_structure/test/test_graph.cc
+++ data_structure/graph_structure/test/test_graph.cc
@@ -2,13 +2,16 @@
 
 using namespace std;
 
+#define INF 0xFFFFFFFF
+int n = 4;
+
 void swap(int a, int b) {
     int temp = a;
     a = b;
     b = temp;
 }
 
-void swapVetex(int *vetexs, int VetexSize, int **mat, int start) {
+void swapVetex(char vetexs[], int VetexSize, int mat[][4], int start) {
     swap(vetexs[0], vetexs[start]);
 
     for(int i = 0; i < VetexSize; ++i)
@@ -18,7 +21,7 @@ void swapVetex(int *vetexs, int VetexSize, int **mat, int start) {
         swap(mat[i][start], mat[i][0]);
 }
 
-void display(int *mat[], int rows, int cols) {
+void display(int mat[][4], int rows, int cols) {
     for(int i = 0; i < rows; ++i) {
         for(int j = 0; j < cols; ++j) {
             cout << mat[i][j] << " ";
@@ -27,13 +30,38 @@ void display(int *mat[], int rows, int cols) {
     }
 }
 
+void BFSN(char V[], int mat[][4]) {
+    for(int i = 0; i < n; ++i) {
+        for(int j = i; j < n; ++j) {
+            if(mat[j][i] != 0 && mat[j][i] != INF) {
+                printf("%c->%c weight=%d\t", V[i], V[j], mat[j][i]);
+            }
+        }
+        cout << endl;
+    }
+}
+
+void DFSN(char V[], int mat[][4]) {
+    for(int i = 0; i < n; ++i) {
+        for(int j = i; j < n; ++j) {
+            if(mat[i][j] != 0 && mat[i][j] != INF) {
+                printf("%c->%c weight=%d", V[j], V[i], mat[i][j]);
+            }
+        }
+        cout << endl;
+    }
+}
+
 int main() {
-    char X[4] = {'A', 'B', 'C', 'D'};
+    char V[4] = {'A', 'B', 'C', 'D'};
     int mat[][4] = {
         {0, 2, 5, 7},
         {2, 0, 6, 3},
         {5, 6, 0, 1},
-        {7, 3, 1, 0}};
-        // display(mat, 4, 4);
+        {7, 3, 1, 0}
+    };
+    swapVetex(V, sizeof(V)/sizeof(char), mat, 3);
+    display(mat, 4, 4);
+    BFSN(V, mat);
     return 0;
 }
